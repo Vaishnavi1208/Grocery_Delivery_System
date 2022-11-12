3b8b959e10c4dfc35e0be51607f1d66c1cde6d72
Vaishnavi1208 <69864877+Vaishnavi1208@users.noreply.github.com>
2022-11-12 17:43:25 +0530

    Delete grocery_delivery_system_backend directory

--- grocery_delivery_system_backend/src/main/java/com/project/grocery_delivery_system/repository/IAddressRepository.java
+++
@@ -1,8 +0,0 @@
-package com.project.grocery_delivery_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.project.grocery_delivery_system.entities.Address;
-
-public interface IAddressRepository extends JpaRepository<Address, Long> {
-
-}
--- grocery_delivery_system_backend/src/main/java/com/project/grocery_delivery_system/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.grocery_delivery_system.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.grocery_delivery_system.entities.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>{
-
-}
